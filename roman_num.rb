47d167d4fd0eb7587adb03fbae95e93e4ca5afdf
clode <uwaclode@gmail.com>
2021-06-15 05:01:25 +0100

    Create method convert and write 5 test cases

--- roman_num.rb
+++ roman_num.rb
@@ -1,6 +1,12 @@
 def convert(number)
   if number == 1
     'I'
+  elsif number == 2
+    'II'
+  elsif number == 3
+    'III'
+  elsif number == 4
+    'IV'
   else
     ''
   end
--- spec/roman_num_spec.rb
+++ spec/roman_num_spec.rb
@@ -7,4 +7,13 @@ describe 'Roman numerals' do
   it 'converts number 1 to a "I"' do
     expect(convert(1)).to eq('I')
   end
+  it 'converts number 2 to a "II"' do
+    expect(convert(2)).to eq('II')
+  end
+  it 'converts number 3 to a "III"' do
+    expect(convert(3)).to eq('III')
+  end
+  it 'converts number 4 to a "IV"' do
+    expect(convert(4)).to eq('IV')
+  end
 end
